0e5d06f6e8d81aa6f2fda237118a2b650e045f2b
h4dri <adrianmalik333@gmail.com>
2020-11-27 21:25:32 +0100

    Add details activity

---
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package coddiers.hackyeah.dziki.ui.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import coddiers.hackyeah.dziki.R
+import kotlinx.android.synthetic.main.activity_details.*
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/coddiers/hackyeah/dziki/ui/notifications/NotificationsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coddiers.hackyeah.dziki.R
+import kotlinx.android.synthetic.main.fragment_notifications.view.*
 
 class NotificationsFragment : Fragment() {
 
@@ -19,13 +20,7 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
